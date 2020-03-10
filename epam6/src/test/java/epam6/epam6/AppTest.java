c797bdf4411e28716c309849b0f367f1658bb9bc
Bhavana5599 <p.bhavanareddy99@gmail.com>
2020-03-10 23:59:13 +0530

    commit

---
+++ epam6/src/test/java/epam6/epam6/AppTest.java
@@ -0,0 +1,12 @@
+package epam6.epam6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
