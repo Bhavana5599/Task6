59c9cc240188c2e5efd082b965d84a3203907ba4
Bhavana5599 <p.bhavanareddy99@gmail.com>
2020-04-03 11:55:32 +0530

    commit

---
+++ EpTa6/src/main/java/EpTa6/EpTa6/App.java
@@ -0,0 +1,13 @@
+package EpTa6.EpTa6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
